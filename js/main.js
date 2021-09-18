24d4926fad58d84245e7143d433aed2070f6bf06
arrecar <arr15004@byui.edu>
2021-09-18 11:52:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,9 +6,18 @@ const links = [
   ]
 
   var arrayLength = links.length;
-for (var i = 0; i < arrayLength; i++){
-    var x = document.createElement("LI");
-    var y = document.createTextNode(links[i]);
-    x.appendChild(y);
-    document.getElementById("list").appendChild(x);
-}
+  //loop through array above
+  for (var i = 0; i < arrayLength; i++){
+        //create li element, to add an item to the list
+          var x = document.createElement("LI");
+          //create an a tag, to add a hyperlink
+          var a = document.createElement("a");
+          //grab the value of the first property of the object and add it as text
+          var y = document.createTextNode(links[i].label);
+          //append the text to the li
+          x.appendChild(y);
+          //set the link to the hyperlink
+          a.setAttribute("href",links[i].url);
+          //insert the item n the list with the lnk
+          document.getElementById("list").appendChild(a).appendChild(x);
+  }
