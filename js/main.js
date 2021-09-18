d669d025a0943ac98881b151f17ac86e40db20fd
arrecar <arr15004@byui.edu>
2021-09-18 11:12:14 -0600

    links list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+const links = [
+    {
+      label: "Week1",
+      url: "https://github.com/arrecar/wdd330/tree/main/js"
+    }
+  ]
 
+  var arrayLength = links.length;
+for (var i = 0; i < arrayLength; i++){
+    var x = document.createElement("LI");
+    var y = document.createTextNode(links[i]);
+    x.appendChild(y);
+    document.getElementById("list").appendChild(x);
+}
